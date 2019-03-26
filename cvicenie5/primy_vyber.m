7427f19155bdc5d4727b551ecf9e6f1c6483dceb
masli <dominika.maslarova@gmail.com>
2019-03-26 23:09:10 +0100

    zmeny cv.5

---
+++ cvicenie5/primy_vyber.m
@@ -0,0 +1,11 @@
+A = [3,4,2,1,6,5]; % Pole, ktore budeme triedit
+n = length(A);     % Dlzka pola A 
+
+for i=2:n % Budeme v cykle prechadzat cele pole, prvy prvok nechame na mieste
+    aktualniHodnota = A(i); % Ulozime si aktualnu hodnotu prvku pola A
+    
+    %DOPLNTE CYKLUS, ktory posuva premennu aktualniHodnota na spravne miesto
+    %v doposial utriedenom poli 
+    
+end
+
