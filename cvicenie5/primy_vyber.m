52fa9dc47632c0f7f4159c306ef875aae435c42f
masli <dominika.maslarova@gmail.com>
2019-03-29 15:23:16 +0100

    zmeny cv.5

--- cvicenie5/primy_vyber.m
+++ cvicenie5/primy_vyber.m
@@ -1,11 +1,27 @@
-A = [3,4,2,1,6,5]; % Pole, ktore budeme triedit
-n = length(A);     % Dlzka pola A 
+A=[3 4 5 1 6 2];
+n=length(A);
 
-for i=2:n % Budeme v cykle prechadzat cele pole, prvy prvok nechame na mieste
-    aktualniHodnota = A(i); % Ulozime si aktualnu hodnotu prvku pola A
-    
-    %DOPLNTE CYKLUS, ktory posuva premennu aktualniHodnota na spravne miesto
-    %v doposial utriedenom poli 
-    
+for i=2:n
+   j = i-1;
+   aktualniHodnota = A(i);
+   
+   while (j>0) && (aktualniHodnota<A(j))
+     A(j+1) = A(j); 
+     A(j)   = aktualniHodnota;
+      j      = j-1;
+   end   
+   
+   %Iny sposob ako zapisat vnutorny cyklus
+   %{
+   for j=i-1:-1:1
+       if A(j)<aktualniHodnota
+           break;
+       end
+       A(j+1)=A(j);
+       A(j)   = aktualniHodnota;
+   end   
+   %}
+   
 end
 
+A
