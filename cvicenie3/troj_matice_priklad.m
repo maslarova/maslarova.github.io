87ccb13a529b1e227e9324b883c758aaa894cce9
Dominika <dominika.maslarova@gmail.com>
2020-03-05 11:08:28 +0100

    3 cviko

--- cvicenie3/troj_matice_priklad.m
+++ cvicenie3/troj_matice_priklad.m
@@ -1,6 +1,6 @@
 %Riesime sustavu A*x=y s maticou A v trojuholnikovom tvare
 A = [8,-1,-2;0,55,-10;0,0,1]; %Matica v trojuholnikovom tvare
-y = [0,80,3]'; %Vektor y
+b = [0,80,3]'; % Vektor b - prava strana
 
 x = NaN(3,1); %Zostrojime vektor x s 3 prvkami nedefinovanych hodnot
 % Hodi sa to na predalokovanie, inak by sme museli velkost vektora stale
@@ -8,9 +8,9 @@ x = NaN(3,1); %Zostrojime vektor x s 3 prvkami nedefinovanych hodnot
 n = length(x); %dlzka vektoru x
 
 %DOPLNTE telo cyklu, ktory postupne pocita zlozky vektoru x3,x2,x1
-for k = length(x):-1:1 %Iterujeme od posledneho prvku vektoru
+for k = n:-1:1 %Iterujeme od posledneho prvku vektoru
 
 end    
     
 x % Vypise x
-linsolve(A,y) %Matlabovske riesenie
+linsolve(A,b) %Matlabovske riesenie
